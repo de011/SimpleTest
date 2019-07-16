5dcef17d7a28d2659fb432ac36563215226fc272
40004085 <40004085@BLRMIESPC-714.lnties.com>
2019-07-16 15:05:22 +0530

    for loop added

--- SimpleTest/src/com/dipu/beans/Demo.java
+++ SimpleTest/src/com/dipu/beans/Demo.java
@@ -3,6 +3,11 @@ package com.dipu.beans;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("this is dipuu!");
+		String str="Deepak";
+		 for(int i=0; i<=10; i++)
+		 {
+			 System.out.println(str);
+		 }
 	}
 
 }
